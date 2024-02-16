2d9873e424c05c51845973936375021f783ffa61
Farhad Maten <farhad.maten@gmail.com>
2024-02-16 15:12:47 -0500

    added social sign-on with google

--- README.md
+++ README.md
@@ -2,16 +2,25 @@
 
 ### Prerequisites
 1. Generate self-signed certificate for SSL (openssl should be installed prior):
-`openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365`
-
-`key.pem` - name of a key file
-`cert.pem` - name of a certificate file
+    - `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365`
+    - `key.pem` - name of a key file
+    - `cert.pem` - name of a certificate file
 
 2. [Helmet.js](https://helmetjs.github.io/)
-Middleware to secure Node.js web server
+    - Middleware to secure Node.js web server
+
+3. Integration with [Google OAuth](https://developers.google.com/identity/protocols/oauth2)
+
+4. Usage of [Passport.js](https://www.passportjs.org/)
+    - Simple authentification middleware for Node.js
+    - `npm install passport-google-oauth20`
+
+5. User session management
+    - server-side: [express-session] (https://expressjs.com/en/resources/middleware/session.html)
+    - client-side: [cookie-session] (https://expressjs.com/en/resources/middleware/cookie-session.html)
 
 ### How to deploy and start the app
 1. Install dependencies:
-`npm install`
+    - `npm install`
 2. Start the app:
-`npm start`
+    - `npm start`
--- server.js
+++ server.js
@@ -3,19 +3,114 @@ const path = require('path');
 const https = require('https');
 const helmet = require('helmet');
 const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const { Strategy } = require('passport-google-oauth20');
+// const ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
+// const ensureLoggedIn = ensureLogIn();
 
-const app = express();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+const config = {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    COOKIE_KEY1: process.env.COOKIE_KEY1,
+    COOKIE_KEY2: process.env.COOKIE_KEY2,
+}
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+}
+// the verifyCallback function allow to save profile information
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+    // console.log("Google profile: ", profile);
+    done(null, profile); // first argument if any error exists, second argument - ggogle profile
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+//Save a session into a cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);  //setting cookie
+})
+//Read a session from a cookie
+passport.deserializeUser((id, done) => {
+    done(null, id); // extract from cookie
+})
+
+const app = express();
+
 app.use(helmet());
+app.use(cookieSession({
+    name: 'mysession',
+    maxAge: 24 * 60 * 60 * 1000, // seconds
+    keys: [ config.COOKIE_KEY1, config.COOKIE_KEY2 ], // secret key for sign a cookie
+}));
+
+// this piece of code required to ensure `cookie-session` will correctly work with `passport.session`
+app.use((req, res, next) => {
+    if (req.session && !req.session.regenerate) {
+        req.session.regenerate = (cb) => {
+            cb();
+        }
+    }
+    if (req.session && !req.session.save) {
+        req.session.save = (cb) => {
+            cb();
+        }
+    }
+    next();
+})
+// end of the code
+
+app.use(passport.initialize());
+app.use(passport.session()); // setting `req.user` object. The user is from passport.deserializeUser function
+
+const checkLoggedIn = (req, res, next) => { // receives req.user from passport.session
+    // console.log('current user: ', req.user);
+    const isLoggedIn = req.isAuthenticated() && req.user;
+    if (!isLoggedIn) {
+        return res.status(401).json({
+            error: "You have to log in"
+        });
+    }
+    next();
+}
+
+app.get("/auth/google", passport.authenticate('google', {
+    scope: ['email']
+}));
+
+app.get("/auth/google/callback",
+    passport.authenticate('google', {
+        failureRedirect: '/failure',
+        successRedirect: '/',
+        session: true,
+    })
+);
+
+app.get("/auth/logout", (req, res, next) => {
+    req.logout((err) => { //remove req.user and clear any logged in session
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+    res.send("This is your secret value: 42");
+})
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/secret", (req, res) => {
-    res.send("This is your secret value: 42");
+app.get("/failure", (req, res) => {
+    res.send("The use failed to log in");
 })
 
 const server = https.createServer({
